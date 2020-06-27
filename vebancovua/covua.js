9efd69304f30880e46300e2b9591f0915b1f8387
Lê Văn Anh <levananhck2.1@users.noreply.github.com>
2020-06-28 00:09:03 +0700

    vẽ bàn cờ vua

---
+++ vebancovua/covua.js
@@ -0,0 +1,34 @@
+
+//Khai báo canvas
+var canvas = document.querySelector ("#canvas")
+canvas.width = 800
+canvas.height = 800
+canvas.style.border = '2px solid black'
+var bcv = canvas.getContext('2d')
+console.log(bcv)
+//vòng lặp
+for(j=0; j<8; j++){
+    for(i=0; i<8; i++){
+        if(j%2 == 0){
+            if(i%2==0){
+                bcv.fillStyle = "black"
+                bcv.fillRect(i*100, j*100, 100, 100)
+            }
+            else{
+                bcv.fillStyle = "white"
+                bcv.fillRect(i*100, j*100, 100, 100)
+            }
+        }
+        else{
+            if(i%2!=0){
+                bcv.fillStyle = "black"
+                bcv.fillRect(i*100, j*100, 100, 100)
+            }
+            else{
+                bcv.fillStyle = "white"
+                bcv.fillRect(i*100, j*100, 100, 100)
+            }
+        }
+    }
+}
+
